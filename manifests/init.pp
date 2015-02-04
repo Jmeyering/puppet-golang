c0f475451228f22e16a25042c5bd80852f22f461
Jmeyering <jared.meyering@gmail.com>
2015-02-03 22:58:46 -0600

    changing to now allow different user

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,7 +16,7 @@ class golang(
   file { "golang-dir":
     path => $installdir,
     ensure => "directory",
-    owner  => $user
+    owner  => "root"
   }
 
   file { "golang-gopath":
@@ -37,7 +37,7 @@ class golang(
     command => "tar -C ${installdir} -xzf ${tempdir}/go${version}.${platform}.tar.gz",
     path    => [ '/usr/bin', '/bin' ],
     creates => "${installdir}/go",
-    user    => $user,
+    user    => "root",
     require => File["golang-dir"]
   }
 
